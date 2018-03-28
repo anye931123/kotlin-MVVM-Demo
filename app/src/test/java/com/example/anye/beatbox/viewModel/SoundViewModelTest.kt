fed42c8c7431cecef2b25d953ecd0394768dc86d
shiph <1278998669@qq.com>
2018-03-28 18:41:20 +0800

    单元测试

---
+++ app/src/test/java/com/example/anye/beatbox/viewModel/SoundViewModelTest.kt
@@ -0,0 +1,31 @@
+package com.example.anye.beatbox.viewModel
+
+import com.example.anye.beatbox.BeatBox
+import com.example.anye.beatbox.data.Sound
+import org.hamcrest.core.Is
+import org.junit.Before
+
+import org.junit.Assert.*
+import org.junit.Test
+import org.mockito.ArgumentMatchers
+import org.mockito.Mockito.isNotNull
+import org.mockito.Mockito.mock
+
+class SoundViewModelTest {
+
+   private var mBeatBox:BeatBox?=null
+    private val mSound:Sound= Sound("assetPath")
+    private var mSubject:SoundViewModel?=null
+
+    @Before
+    fun setUp() {
+        mBeatBox= mock(BeatBox::class.java)
+        mSubject=SoundViewModel(mBeatBox!!)
+        mSubject!!.mSound=mSound
+    }
+
+    @Test
+    fun exposesSoundNameAsTitle(){
+        assertThat(mSubject!!.getTitle(),Is.`is`(mSound.mName))
+    }
+}
